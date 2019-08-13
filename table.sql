87cfdda5944b0fb66071aa58378f4b3ed8ac1d8d
oldybloke <fuzzzywuzzzy@outlook.com>
2019-08-13 11:39:08 -0400

    Add SQL Table for example

---
+++ table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `spawntest` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `STATUS` char(1) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM;
+
+INSERT INTO spawntest (ID,STATUS) VALUES(1,'N'),(2,'N')(3,'N'),(4,'N'),(5,'N'),(6,'N'),(7,'N'),(8,'N'),(9,'N'),(10,'N'),(11,'N'),(12,'N'),(13,'N'),(14,'N'),(15,'N'),(16,'N'),(17,'N'),(18,'N'),(19,'N'),(20,'N'),(21,'N'),(22,'N'),(23,'N'),(24,'N'),(25,'N'),(26,'N'),(27,'N'),(28,'N'),(29,'N'),(30,'N');
